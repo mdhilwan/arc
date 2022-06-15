749e59a3380c92d991b27b5c761fc6585106d893
Md Hilwan <muhammadhilwan.mohamedidrus@mastercard.com>
2022-06-15 11:34:41 -0500

    change header text

--- src/app/component/login-form/login-form.component.ts
+++ src/app/component/login-form/login-form.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { FormControl, FormGroup } from "@angular/forms";
-import { Router } from "@angular/router";
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-form',
