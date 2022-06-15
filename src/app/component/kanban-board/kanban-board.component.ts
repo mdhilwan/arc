d47d658714fe02f0638b12b12599cb252f89496d
Md Hilwan <muhammadhilwan.mohamedidrus@mastercard.com>
2022-06-15 11:30:01 -0500

    update kanban board

--- src/app/component/kanban-board/kanban-board.component.ts
+++ src/app/component/kanban-board/kanban-board.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { animalInterface } from '../../interface/animal.interface';
+import { purposeTagInterface } from '../../interface/purposeTag.interface';
 
 @Component({
   selector: 'app-kanban-board',
@@ -7,6 +10,52 @@ import { Component } from '@angular/core';
 })
 export class KanbanBoardComponent {
 
+  purposeA: purposeTagInterface = {
+    purpose: 'CPP',
+    label: 'CPP'
+  }
+
+  purposeTags: purposeTagInterface[] = [this.purposeA]
+
+  animal: animalInterface = {
+    animalNumber: '1',
+    sex: 'M',
+    parent: '2',
+    cageNumber: 'cage 2',
+    location: 'loc 2',
+    purposeTag: this.purposeA,
+    dob: new Date()
+  }
+
+  newAnimals: animalInterface[] = [this.animal, this.animal];
+
+  inProgressAnimals: animalInterface[] = [];
+
+  sacrificedAnimals: animalInterface[] = [];
+
+  sectionedAnimals: animalInterface[] = [];
+
+  mountedAnimals: animalInterface[] = [];
+
+  cryoProtectAnimals: animalInterface[] = [];
+
   constructor() { }
+
+  drop(event: CdkDragDrop<animalInterface[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
 
 }
