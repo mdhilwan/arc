901a104489fa6af7f6c202d21d00528fc0492f33
Md Hilwan <muhammadhilwan.mohamedidrus@mastercard.com>
2022-06-15 11:31:36 -0500

    update app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,21 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './page/home/home.component';
 import { LoginFormComponent } from './component/login-form/login-form.component';
-import { MatCardModule } from "@angular/material/card";
-import { MatInputModule } from "@angular/material/input";
-import { MatButtonModule } from "@angular/material/button";
-import { ReactiveFormsModule } from "@angular/forms";
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
 import { DashboardComponent } from './page/dashboard/dashboard.component';
 import { KanbanBoardComponent } from './component/kanban-board/kanban-board.component';
-import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { HeaderComponent } from './component/header/header.component';
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatListModule } from "@angular/material/list";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 import { SideNavComponent } from './component/side-nav/side-nav.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { AnimalCardComponent } from './component/kanban-board/animal-card/animal-card.component';
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
@@ -26,7 +30,8 @@ import { SideNavComponent } from './component/side-nav/side-nav.component';
     DashboardComponent,
     KanbanBoardComponent,
     HeaderComponent,
-    SideNavComponent
+    SideNavComponent,
+    AnimalCardComponent
   ],
   imports: [
     BrowserModule,
@@ -38,7 +43,10 @@ import { SideNavComponent } from './component/side-nav/side-nav.component';
     BrowserAnimationsModule,
     MatSidenavModule,
     MatToolbarModule,
-    MatListModule
+    MatListModule,
+    DragDropModule,
+    MatButtonToggleModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
